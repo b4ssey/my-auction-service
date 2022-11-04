234269f0097190ad9e711dcd39b3ae08295261e0
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-11-04 15:23:55 +0000

    created commonMiddleware

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -1,9 +1,6 @@
 import AWS from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
+import commonMiddleware from "../libs/commonMiddleware";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -35,7 +32,4 @@ async function getAuction(event, context) {
   };
 }
 
-export const handler = middy(getAuction)
-  .use(httpErrorHandler())
-  .use(httpEventNormalizer())
-  .use(httpJsonBodyParser());
+export const handler = commonMiddleware(getAuction);
