85f9b89f97b6ecc7066905f97db41e444153683c
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-11-09 09:59:17 +0000

    try/catch => uploadActioPicture

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,3 +1,6 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 import { getAuctionById } from "./getAuction";
 
@@ -6,12 +9,21 @@ export async function uploadAuctionPicture(event) {
   const auction = await getAuctionById(id);
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
-  const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
-  console.log(uploadToS3Result);
+  try {
+    const uploadToS3Result = await uploadPictureToS3(
+      auction.id + ".jpg",
+      buffer
+    );
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
